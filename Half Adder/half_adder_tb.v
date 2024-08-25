83d267863466d43b14fe54ed9f2ece84c69784f7
inish1504 <120587807+inish1504@users.noreply.github.com>
2024-08-25 17:33:11 +0530

    Update half_adder_tb.v

--- Half Adder/half_adder_tb.v
+++ Half Adder/half_adder_tb.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/25/2024 01:40:12 PM
-// Design Name: 
-// Module Name: half_adder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module half_adder_tb();
 reg t_a,t_b;
 wire SUM,CARRY;
