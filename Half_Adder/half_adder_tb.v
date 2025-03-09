d169b2fc37541c8f2a9a2a292d69ac438aefbc37
Inish <inish1504@gmail.com>
2025-03-10 03:46:55 +0530

    Added dumpfile to store the variables

--- Half_Adder/half_adder_tb.v
+++ Half_Adder/half_adder_tb.v
@@ -4,6 +4,8 @@ module half_adder_tb();
 
     half_adder dut(.a(t_a),.b(t_b),.sum(s),.carry(c));
     initial begin
+        $dumpfile("half_adder_tb.vcd"); 
+        $dumpvars(0, half_adder_tb);    
         t_a=0;t_b=0;
         #10
         t_a=0;t_b=1;
