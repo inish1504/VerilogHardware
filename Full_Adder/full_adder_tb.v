629efd2a6ec6d5f75f3ecd8abeefa53c00ecd1b8
Inish <inish1504@gmail.com>
2025-03-10 19:16:51 +0530

    Created Test Bench for Full Adder

---
+++ Full_Adder/full_adder_tb.v
@@ -0,0 +1,28 @@
+module full_adder_tb();
+    reg t_a,t_b,t_c;
+    wire SUM,CARRY_OUT;
+
+    full_adder dut(.a(t_a),.b(t_b),.c(t_c),.s(SUM),.carry(CARRY_OUT));
+
+    initial begin
+        $dumpfile("full_adder_tb.vcd");
+        $dumpvars(0, full_adder_tb);
+        t_a=0;t_b=0;t_c=0;
+        #10
+        t_a=0;t_b=0;t_c=1;
+        #10
+        t_a=0;t_b=1;t_c=0;
+        #10
+        t_a=0;t_b=1;t_c=1;
+        #10
+        t_a=1;t_b=0;t_c=0;
+        #10
+        t_a=1;t_b=0;t_c=1;
+        #10
+        t_a=1;t_b=1;t_c=0;
+        #10
+        t_a=1;t_b=1;t_c=1;
+        #10
+        $stop;
+    end
+endmodule
