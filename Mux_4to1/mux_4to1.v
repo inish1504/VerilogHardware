f6b13ef789322b0ba4b8d899457d488317e8bea5
Inish <inish1504@gmail.com>
2025-03-14 01:56:12 +0530

    Created 4:1 Mux module

---
+++ Mux_4to1/mux_4to1.v
@@ -0,0 +1,16 @@
+module mux_4to1 (
+    input wire [1:0] sel,
+    input wire [3:0] in,
+    output reg out
+);
+    always @(*)
+    begin
+        case (sel)
+            2'b00: out = in[0];
+            2'b01: out = in[1];
+            2'b10: out = in[2];
+            2'b11: out = in[3];
+            default: out = 0;
+        endcase
+    end
+endmodule
