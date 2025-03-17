2c430e7f43a3ccfdfeae8d50af4a87a53e18a434
Inish <inish1504@gmail.com>
2025-03-18 03:24:39 +0530

    Update: monitor statement included

--- Decoder_2to4/decoder_2to4_tb.v
+++ Decoder_2to4/decoder_2to4_tb.v
@@ -12,6 +12,7 @@ module decoder_2to4_tb;
     initial begin
         $dumpfile("decoder_2to4_tb.vcd");
         $dumpvars(0, decoder_2to4_tb);
+        $monitor("Time=%0t | Enable=%b | in=%b | out=%b", $time, enable, in, out);
 
         enable = 0; in = 2'b00; #10;
         enable = 1; in = 2'b00; #10;
